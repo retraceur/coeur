8a61eafb7de0db4238c9bd3b0a7e4489ddff4d46
imath <math.viet@gmail.com>
2025-02-05 00:07:10 +0100

    Explain how to contribute to Retraceur
    Fixes #50

--- wp-admin/contribute.php
+++ wp-admin/contribute.php
@@ -37,30 +37,93 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 		<a href="contribute.php" class="nav-tab nav-tab-active" aria-current="page"><?php esc_html_e( 'Contributing' ); ?></a>
 	</nav>
 
-	<div class="about__section has-1-column">
+	<div class="about__section is-feature">
+		<div class="column aligncenter">
+			<p class="is-subheading">
+				<?php
+				printf(
+					/* Translators: %s: Heart emoji. */
+					esc_html__( 'First off, thanks for taking the time to contribute %s!' ),
+					wp_staticize_emoji( '♥️' )
+				);
+				?>
+			</p>
+		</div>
+	</div>
+
+	<div class="about__section">
 		<div class="column">
-			<h2 class="is-smaller-heading"><?php _e( 'No-code contribution' ); ?></h2>
-			<p><?php _e( 'Retraceur may thrive on technical contributions, but you don&#8217;t have to code to contribute. Here are some of the ways you can make an impact without writing a single line of code:' ); ?></p>
+			<p>
+				<?php printf(
+					/* Translators: 1: Retraceur Cœeur GitHub repository URL. 2: Retraceur documentation GitHub repository URL. 3: Retraceur Cœeur GitHub issues URL. 4: Retraceur Cœeur GitHub PR URL. */
+					esc_html__( 'All types of contributions are encouraged and valued: you can contribute with %1$s, %2$s, translations, testing pre-releases, %3$s and %4$s.' ),
+					'<a href="https://github.com/retraceur/coeur">' . __( 'code' ) . '</a>',
+					'<a href="https://github.com/retraceur/retraceur.github.io">' . __( 'documentation' ) . '</a>',
+					'<a href="https://github.com/retraceur/coeur/issues">' . __( 'reporting issues' ) . '</a>',
+					'<a href="https://github.com/retraceur/coeur/pulls">' . __( 'suggesting pull requests' ) . '</a>'
+				);
+				?>
+			</p>
+			<p><?php esc_html_e( 'And if you like the project, but just don’t have time to contribute, that’s fine. There are other easy ways to support the project and show your appreciation, which we would also be very happy about:' ); ?></p>
 			<ul>
-				<li><?php _e( '<strong>Write</strong> or improve documentation for Retraceur.' ); ?></li>
-				<li><?php _e( '<strong>Translate</strong> Retraceur into your local language.' ); ?></li>
-				<li><?php _e( '<strong>Explore</strong> ways to reduce the environmental impact of websites.' ); ?></li>
+				<li>
+					<?php
+					printf(
+						/* Translators: 1: Star emoji. 2: Retraceur Cœeur GitHub repository URL. */
+						esc_html__( 'Star %1$s the project on %2$s.' ),
+						wp_staticize_emoji( '⭐️' ),
+						'<a href="https://github.com/retraceur/coeur">GitHub</a>'
+					);
+					?>
+				</li>
+				<li>
+					<?php
+					printf(
+						/* Translators: %s: Retraceur Bluesky profile URL. */
+						esc_html__( 'Follow %s on Bluesky and/or share posts about it.' ),
+						'<a href="https://bsky.app/profile/retraceur.bsky.social">Retraceur</a>'
+					);
+					?>
+				</li>
+				<li><?php esc_html_e( 'Mention the project at local meetups and tell your friends/colleagues' ); ?></li>
 			</ul>
 		</div>
 	</div>
-	<div class="about__section has-1-column">
+
+	<div class="about__section">
 		<div class="column">
-			<h2 class="is-smaller-heading"><?php _e( 'Code-based contribution' ); ?></h2>
-			<p><?php _e( 'If you do code, or want to learn how, you can contribute technically in numerous ways:' ); ?></p>
-			<ul>
-				<li><?php _e( '<strong>Find</strong> and report bugs in the Retraceur core software.' ); ?></li>
-				<li><?php _e( '<strong>Test</strong> new releases and proposed features for the Block Editor.' ); ?></li>
-				<li><?php _e( '<strong>Write</strong> and submit patches to fix bugs or help build new features.' ); ?></li>
-			</ul>
-			<p><?php _e( 'Retraceur embraces new technologies, while being committed to backward compatibility. The Retraceur project uses the following languages and libraries:' ); ?></p>
-			<ul>
-				<li><?php _e( 'Retraceur Core and Block Editor: HTML, CSS, PHP, SQL, JavaScript, and React.' ); ?></li>
-			</ul>
+			<h2><?php esc_html_e( 'Code of conduct' ) ;?></h2>
+			<p>
+				<?php
+				printf(
+					/* Translators: %s: Retraceur Code of conduct link. */
+					esc_html__( 'This project and everyone participating in it is governed by the %s. By participating, you are expected to uphold this code.' ),
+					'<a href="' . esc_url( __( 'https://retraceur.github.io/rules/code-of-conduct/' ) ) . '">' . esc_html__( 'Retraceur Code of Conduct' ) . '</a>'
+				);
+				?>
+			</p>
+		</div>
+	</div>
+
+	<div class="about__section">
+		<div class="column">
+			<h2><?php esc_html_e( 'Legal Notice' ) ;?></h2>
+			<p><?php esc_html_e( 'When contributing to this project, you must agree that you have authored 100% of the content, that you have the necessary rights to the content and that the content you contribute may be provided under the GPL.' ); ?></p>
+		</div>
+	</div>
+
+	<div class="about__section">
+		<div class="column">
+			<h2><?php esc_html_e( 'Reporting security issues' ) ;?></h2>
+			<p>
+				<?php
+				printf(
+					/* Translators: %s: Email address to report security issues. */
+					esc_html__( 'You must never report security related issues, vulnerabilities or bugs including sensitive information to the issue tracker, or elsewhere in public. Instead sensitive bugs must be sent by email to <%s>.' ),
+					'<a href="mailto:retraceur+security@proton.me">retraceur+security@proton.me</a>'
+				);
+				?>
+			</p>
 		</div>
 	</div>
 
