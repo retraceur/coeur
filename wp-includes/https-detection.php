8ab35e5baabac0b0d44beb1802b1b9b239fb13d9
imath <math.viet@gmail.com>
2025-04-27 13:22:44 +0200

    Include 4 6.8-branch commits
    Commit list: 87e7f72397ac456d6fdd4a3d451df427c8199953
    1bf26605ca831937ac553e5df0e58d8df54c23f4
    f7d4ea3ceb99ae4d35fe4982f06ac65548706301
    57e024a15760bd0721a4a97fdd8986a802f58132
    
    See #60

--- wp-includes/https-detection.php
+++ wp-includes/https-detection.php
@@ -65,28 +65,28 @@ function wp_is_site_url_using_https() {
 /**
  * Checks whether HTTPS is supported for the server and domain.
  *
+ * This function makes an HTTP request through `wp_get_https_detection_errors()`
+ * to check for HTTPS support. As this process can be resource-intensive,
+ * it should be used cautiously, especially in performance-sensitive environments,
+ * to avoid potential latency issues.
+ *
  * @since WP 5.7.0
  *
  * @return bool True if HTTPS is supported, false otherwise.
  */
 function wp_is_https_supported() {
-	$https_detection_errors = get_option( 'https_detection_errors' );
+	$https_detection_errors = wp_get_https_detection_errors();
 
-	// If option has never been set by the Cron hook before, run it on-the-fly as fallback.
-	if ( false === $https_detection_errors ) {
-		wp_update_https_detection_errors();
-
-		$https_detection_errors = get_option( 'https_detection_errors' );
-	}
-
-	// If there are no detection errors, HTTPS is supported.
+	// If there are errors, HTTPS is not supported.
 	return empty( $https_detection_errors );
 }
 
 /**
  * Runs a remote HTTPS request to detect whether HTTPS supported, and stores potential errors.
  *
- * This internal function is called by a regular Cron hook to ensure HTTPS support is detected and maintained.
+ * This function checks for HTTPS support by making an HTTP request. As this process can be resource-intensive,
+ * it should be used cautiously, especially in performance-sensitive environments.
+ * It is called when HTTPS support needs to be validated.
  *
  * @since WP 6.4.0
  * @access private
