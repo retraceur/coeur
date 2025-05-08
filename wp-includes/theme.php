39d27f3d72b581febda1b1c7f756bc1c384433ff
imath <math.viet@gmail.com>
2025-05-08 09:25:22 +0200

    Include 4 6.8-branch commits
    Commit list:
    47efd20a71fbf066e12f97e4fdb5c05d13966769
    1a9494258555fdf2af0e79e079f3e831c352b8af
    7244fe456e5ade2bb57ef6df3b8d224284382966
    ec604f4ae04b8f7ced14fd6ecb2263b350ade2f0
    
    See #60

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -3731,4 +3731,5 @@ function _add_default_theme_supports() {
 	add_theme_support( 'automatic-feed-links' );
 
 	add_filter( 'should_load_separate_core_block_assets', '__return_true' );
+	add_filter( 'should_load_block_assets_on_demand', '__return_true' );
 }
