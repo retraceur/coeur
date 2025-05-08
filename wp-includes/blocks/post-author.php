4c504f333047ce917ba5a64eb6b876ca41a9ae1a
imath <math.viet@gmail.com>
2025-05-08 11:17:58 +0200

    Update remaining blocks
    See #61

--- wp-includes/blocks/post-author.php
+++ wp-includes/blocks/post-author.php
@@ -28,6 +28,10 @@ function render_block_core_post_author( $attributes, $content, $block ) {
 		return '';
 	}
 
+	if ( ! post_type_supports( $block->context['postType'], 'author' ) ) {
+		return '';
+	}
+
 	$avatar = ! empty( $attributes['avatarSize'] ) ? get_avatar(
 		$author_id,
 		$attributes['avatarSize']
