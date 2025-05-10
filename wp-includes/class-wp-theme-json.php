9683fdb4fd2b7c15eea058272453335604b87936
imath <math.viet@gmail.com>
2025-05-11 01:19:52 +0200

    Include 4 6.8-branch commits
    Commit list:
    690d90478656377b7f1ea027e2ddaf3b025751e5
    7f13d32bc2139221766c3db82b40acba635396bb
    54734b4396fb949b2feda16fd5c00c7093accfaa
    20c3c4fdff615bbcf47d6b64d79bd4e0cc7fafb4
    
    See #60

--- wp-includes/class-wp-theme-json.php
+++ wp-includes/class-wp-theme-json.php
@@ -580,7 +580,7 @@ class WP_Theme_JSON {
 	/**
 	 * Defines which pseudo selectors are enabled for which elements.
 	 *
-	 * The order of the selectors should be: link, any-link, visited, hover, focus, active.
+	 * The order of the selectors should be: link, any-link, visited, hover, focus, focus-visible, active.
 	 * This is to ensure the user action (hover, focus and active) styles have a higher
 	 * specificity than the visited styles, which in turn have a higher specificity than
 	 * the unvisited styles.
@@ -589,11 +589,12 @@ class WP_Theme_JSON {
 	 *
 	 * @since WP 6.1.0
 	 * @since WP 6.2.0 Added support for ':link' and ':any-link'.
+	 * @since WP 6.8.0 Added support for ':focus-visible'.
 	 * @var array
 	 */
 	const VALID_ELEMENT_PSEUDO_SELECTORS = array(
-		'link'   => array( ':link', ':any-link', ':visited', ':hover', ':focus', ':active' ),
-		'button' => array( ':link', ':any-link', ':visited', ':hover', ':focus', ':active' ),
+		'link'   => array( ':link', ':any-link', ':visited', ':hover', ':focus', ':focus-visible', ':active' ),
+		'button' => array( ':link', ':any-link', ':visited', ':hover', ':focus', ':focus-visible', ':active' ),
 	);
 
 	/**
