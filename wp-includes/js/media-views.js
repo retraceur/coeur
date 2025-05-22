3f8dcc32062cd6eadcf5dc5dfeae32da976c6a0e
imath <imathi.eu@outlook.fr>
2025-05-22 20:33:15 +0200

    Include 4 6.8-branch commits
    Commit list:
    8eb3443f3194f4d04a8dadf90a499b497dfb0ecf
    e895a309d3ddd88b449e75e6e389aa5cba9d8fc6
    31c2c95700f9f03a45f7db7605e56fe42720d3a6
    3cc90101d3b00160bb979a377d33ca00fd687ea2
    
    See #60

--- wp-includes/js/media-views.js
+++ wp-includes/js/media-views.js
@@ -9478,7 +9478,7 @@ Toolbar = View.extend(/** @lends wp.media.view.Toolbar.prototype */{
 
 			var requires = button.options.requires,
 				disabled = false,
-				modelsUploading = ! _.isEmpty( library.findWhere(  { 'uploading': true } ) );
+				modelsUploading = library && ! _.isEmpty( library.findWhere(  { 'uploading': true } ) );
 
 			// Prevent insertion of attachments if any of them are still uploading.
 			if ( selection && selection.models ) {
@@ -10379,7 +10379,7 @@ module.exports = View;
 /************************************************************************/
 /******/ 	// The module cache
 /******/ 	var __webpack_module_cache__ = {};
-/******/ 	
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/ 		// Check if module is in cache
@@ -10393,14 +10393,14 @@ module.exports = View;
 /******/ 			// no module.loaded needed
 /******/ 			exports: {}
 /******/ 		};
-/******/ 	
+/******/
 /******/ 		// Execute the module function
 /******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/ 	
+/******/
 /************************************************************************/
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
