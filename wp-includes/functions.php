bb1701248d96ee596fc12e23f69aa11956c3f6f8
imath <imathi.eu@outlook.fr>
2025-05-22 20:55:02 +0200

    Include 4 6.8-branch commits
    Commit list:
    886fb3120798f7cc8f576e884588e949ac297f72
    46cb039f267e0ee09ce8738e33b933ce60243d7b
    94bac1c6b33d365a721c564035d1ed74046af416
    d5a86a55abc6eed007bef923f917b35fb7b3a9a1
    
    See #60

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -8705,11 +8705,10 @@ function wp_unique_id_from_values( array $data, string $prefix = '' ): string {
 	if ( empty( $data ) ) {
 		_doing_it_wrong(
 			__FUNCTION__,
-			sprintf(
-				__( 'The $data argument must not be empty.' ),
-				gettype( $data )
-			),
-			'6.8.0'
+			// translators: $data` is the parameter name and must not be translated.
+			__( 'The $data argument must not be empty.' ),
+			'2.0.0',
+			true
 		);
 	}
 	$serialized = wp_json_encode( $data );
