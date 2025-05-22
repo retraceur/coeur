49c72b713639e2d3411f04afd39f7f665750123a
imath <math.viet@gmail.com>
2025-05-22 05:41:28 +0200

    Include 4 6.8-branch commits
    Commit list:
    1c43cd674396d608c0f29f4246a87d1e9a7506f4
    299978551a2042f5d78ce3ab7f7913316ce0529c
    2ea5c7f332700c6fb74412f932f8fb24bae3497e
    53b38b0516ed7129f5a823c6dd1d8a1ab017993a
    
    See #60

--- wp-includes/theme.php
+++ wp-includes/theme.php
@@ -3688,6 +3688,11 @@ function create_initial_theme_features() {
  * @return bool Whether the active theme is a block-based theme or not.
  */
 function wp_is_block_theme() {
+	if ( empty( $GLOBALS['wp_theme_directories'] ) ) {
+		_doing_it_wrong( __FUNCTION__, __( 'This function should not be called before the theme directory is registered.' ), '2.0.0', true );
+		return false;
+	}
+
 	return wp_get_theme()->is_block_theme();
 }
 
