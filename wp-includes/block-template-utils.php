9eb94f7479a8912acf49e91311d7e0bdf7fb41da
imath <imathi.eu@outlook.fr>
2025-06-07 14:21:19 +0200

    Manage Post format posts slugToCheck so that the right template is found
    The Editor still needs to be edited.
    
    See #1

--- wp-includes/block-template-utils.php
+++ wp-includes/block-template-utils.php
@@ -212,6 +212,18 @@ function get_default_block_template_types() {
 	// Add a title and description to post format templates.
 	$post_formats = get_post_format_strings();
 	foreach ( $post_formats as $post_format_slug => $post_format_name ) {
+		$default_template_types[ 'single-post-format-' . $post_format_slug ] = array(
+			'title'       => sprintf(
+				/* translators: %s: Post format name. */
+				_x( 'Single Post Format entries: %s', 'Template name' ),
+				$post_format_name
+			),
+			'description' => sprintf(
+				/* translators: %s: Post format name. */
+				__( 'Displays the %s post format single entry.' ),
+				$post_format_name
+			),
+		);
 		$default_template_types[ 'taxonomy-post_format-post-format-' . $post_format_slug ] = array(
 			'title'       => sprintf(
 				/* translators: %s: Post format name. */
