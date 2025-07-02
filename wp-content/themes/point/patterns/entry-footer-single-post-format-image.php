808c5f1f69868a46b281772325b3ee1540ee543b
imath <math.viet@gmail.com>
2025-07-02 04:03:43 +0200

    Point: display image post format differently
    See #1

---
+++ wp-content/themes/point/patterns/entry-footer-single-post-format-image.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Point’s image format entry footer
+ * Slug: point/entry-footer-single-post-format-image
+ * Inserter: no
+ *
+ * @package Retraceur
+ * @subpackage Content/Themes/Point
+ *
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","margin":{"top":"var:preset|spacing|40"}},"typography":{"fontSize":"16px"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);font-size:16px">
+	<!-- wp:paragraph -->
+	<p><?php echo esc_html_x( 'Published on', '"Published on" date separator' ); ?></p>
+	<!-- /wp:paragraph -->
+	<!-- wp:post-date /-->
+	<!-- wp:paragraph -->
+	<p><?php echo esc_html_x( 'by', '"by" Author separator' ); ?></p>
+	<!-- /wp:paragraph -->
+	<!-- wp:post-author {"showAvatar":false} /-->
+</div>
+<!-- /wp:group -->
