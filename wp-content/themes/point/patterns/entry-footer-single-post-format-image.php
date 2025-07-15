e1fc63183131f5b80df1263a37f0433114436ff0
imath <math.viet@gmail.com>
2025-07-15 15:14:24 +0200

    Point: improve various layouts
    - Status : make the content looks like a dialog bubble.
    - Make sure archive templates are displaying all post formats content.
    - Do not use post titles in pagination.
    
    See #1

--- wp-content/themes/point/patterns/entry-footer-single-post-format-image.php
+++ wp-content/themes/point/patterns/entry-footer-single-post-format-image.php
@@ -12,7 +12,7 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","margin":{"top":"var:preset|spacing|40"}},"typography":{"fontSize":"16px"}},"layout":{"type":"flex"}} -->
 <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);font-size:16px">
-	<!-- wp:post-format-name {"isLink":false,"level":1,"className":"inline-post-format"} /-->
+	<!-- wp:post-format-name {"isLink":false,"level":1,"className":"post-format-image"} /-->
 	<!-- wp:paragraph -->
 	<p><?php echo esc_html_x( ': published on', 'Image post format "Published on" date separator' ); ?></p>
 	<!-- /wp:paragraph -->
--- wp-content/themes/point/patterns/entry-header-single-post-format-status.php
+++ wp-content/themes/point/patterns/entry-header-single-post-format-status.php
@@ -10,10 +10,9 @@
  * @since 2.0.0
  */
 ?>
-<!-- wp:post-format-name {"isLink":false,"level":1,"className":"screen-reader-text"} /-->
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","margin":{"bottom":"var:preset|spacing|50"}},"typography":{"fontSize":"16px"}},"layout":{"type":"flex"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50);font-size:16px">
-	<!-- wp:post-author {"showAvatar":true, "avatarSize":32, "className":"inline-post-format"} /-->
+	<!-- wp:post-author {"isLink":true, "showAvatar":true, "avatarSize":32, "className":"post-format-status"} /-->
 	<!-- wp:paragraph -->
 	<p><?php echo esc_html_x( 'on', 'Status post format "Published on" date separator' ); ?></p>
 	<!-- /wp:paragraph -->
