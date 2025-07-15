28357502b4663f61a19e8b943e270e250e913095
imath <math.viet@gmail.com>
2025-07-15 11:11:53 +0200

    Point: create new patterns & template for Status post format
    - Rename the `inline-image-format` class in favor of `inline-post-format`.
    - create patterns to display the status post format headers like what is done on social networks (Avatar, name, date above the content).
    - Use the `screen-reader-text` class to preserve headings hierarchy
    
    See #1

--- wp-content/themes/point/patterns/entry-header-archive-post-format-status.php
+++ wp-content/themes/point/patterns/entry-header-archive-post-format-status.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Point’s Post Format entry header
- * Slug: point/entry-header-post-format
+ * Title: Point’s Status Post Format archive entry header
+ * Slug: point/entry-header-archive-post-format-status
  * Inserter: no
  *
  * @package Retraceur
@@ -10,17 +10,13 @@
  * @since 2.0.0
  */
 ?>
+<!-- wp:post-format-name {"isLink":false,"level":2,"className":"screen-reader-text"} /-->
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","margin":{"bottom":"var:preset|spacing|50"}},"typography":{"fontSize":"16px"}},"layout":{"type":"flex"}} -->
 <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--50);font-size:16px">
+	<!-- wp:post-author {"showAvatar":true, "avatarSize":32, "className":"inline-post-format"} /-->
 	<!-- wp:paragraph -->
-	<p><?php echo esc_html_x( 'Published on', '"Published on" date separator' ); ?></p>
+	<p><?php echo esc_html_x( 'on', 'Status post format "Published on" date separator' ); ?></p>
 	<!-- /wp:paragraph -->
-	<!-- wp:post-date /-->
-	<?php if( ! is_author() ) : ?>
-		<!-- wp:paragraph -->
-		<p><?php echo esc_html_x( 'by', '"by" Author separator' ); ?></p>
-		<!-- /wp:paragraph -->
-		<!-- wp:post-author {"showAvatar":false} /-->
-	<?php endif; ?>
+	<!-- wp:post-date {"isLink":true} /-->
 </div>
 <!-- /wp:group -->
