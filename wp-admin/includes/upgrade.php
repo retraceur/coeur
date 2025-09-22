12f7b0c025aae13a8b2347a853d715305603fda3
imath <imathi.eu@outlook.fr>
2025-09-22 14:01:20 +0200

    Include 4 commits from wp 6.9 Commits list: - 7084408a7ebf280d5865868f1dfedecf108272ca - efdd94e890b8922b95ecdddbf60757aaad8d4094 - 85b7808c1f09ca212c3ca783b7e2e6881f90f807 - 7001742fa4ce44bf69991cd613329e4de8ded494
    Trunk is now synced with `6.9-alpha-60237`

--- wp-admin/includes/upgrade.php
+++ wp-admin/includes/upgrade.php
@@ -284,7 +284,7 @@ if ( ! function_exists( 'wp_install_defaults' ) ) :
 
 		// First post.
 		$now             = current_time( 'mysql' );
-		$now_gmt         = current_time( 'mysql', 1 );
+		$now_gmt         = current_time( 'mysql', true );
 		$first_post_guid = get_option( 'home' ) . '/?p=1';
 		$first_post      = "<!-- wp:paragraph -->\n<p>" .
 			/* translators: First post content. */
