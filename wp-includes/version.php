d882a5e5a6df30bce0b88ec0a03a55674a60a436
imath <math.viet@gmail.com>
2025-10-01 20:19:45 +0200

    Do version 2.0.1 bumps

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -27,7 +27,7 @@ $wp_version = '6.8.3';
  *
  * @global string $retraceur_version
  */
-$retraceur_version = '2.0.1-alpha';
+$retraceur_version = '2.0.1';
 
 /**
  * Holds the WP DB revision, increments when changes are made to the WP DB schema.
