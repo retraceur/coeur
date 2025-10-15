51e43bedfeb8894ac33398802c54198c6827e7a3
imath <math.viet@gmail.com>
2025-10-15 23:20:29 +0200

    Include 4 commits from wp 6.9
    Commits list:
    - e2442c0b5b4e6de22eca941a480ae17f70c4d8bd
    - 304382e37df3dc9f13b9899ce7a54b1b2d5e0b36
    - 4f8fc714a4a12e0e8c8974fc4e898ec965b2f7ab
    - 9ab3854c4cd5b5e3f9ef89bcea198a2fd2cc7a82
    
    Trunk is now synced with `6.9-alpha-60648`
    
    See #121

--- wp-includes/author-template.php
+++ wp-includes/author-template.php
@@ -123,12 +123,10 @@ function the_modified_author() {
  * Valid values for the `$field` parameter include:
  *
  * - admin_color
- * - aim
  * - description
  * - display_name
  * - first_name
  * - ID
- * - jabber
  * - last_name
  * - nickname
  * - plugins_last_view
@@ -147,9 +145,9 @@ function the_modified_author() {
  * - user_registered
  * - user_status
  * - user_url
- * - yim
  *
  * @since WP 2.8.0
+ * @since WP 6.9.0 Removed `aim`, `jabber`, and `yim` as valid values for the `$field` parameter.
  *
  * @global WP_User $authordata The current author's data.
  *
