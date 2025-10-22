5eff2c6686718ba313339ca5933e0d4e69345fb3
imath <math.viet@gmail.com>
2025-10-23 01:25:20 +0200

    Include 4 commits from wp 6.9 Commits list: - cb59d63c60e03be8d16b332d393ee77b75c242ae - 1b4c99fd9253184165d14b78cc917555714dd345 - cd68f370e962d71dea6017b4915a74c2a99bf673 - 275127f8b3940ecf1759721de957435559ea186c
    Trunk is now synced with `6.9-alpha-60713`
    
    See #121

--- wp-includes/user.php
+++ wp-includes/user.php
@@ -2500,7 +2500,7 @@ function wp_insert_user( $userdata ) {
 
 	if ( ! $update ) {
 		/** This action is documented in wp-includes/pluggable.php */
-		do_action( 'wp_set_password', $userdata['user_pass'], $user_id, $userdata );
+		do_action( 'wp_set_password', $userdata['user_pass'], $user_id, $user );
 	}
 
 	/**
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-60709';
+$wp_version = '6.9-alpha-60713';
 
 /**
  * The Retraceur version string.
