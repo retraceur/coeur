e1a0587192df5ea1766f156a7e23fe23291abd5d
imath <math.viet@gmail.com>
2025-11-14 02:15:13 +0100

    Add 4 commits from wp 6.9
    Commits list:
    - d5243f4ddf547bf16a83cbf65155fa1e4f5e2884
    - 10e6cd7ff15431e57eb58711115e6b094cd8f452
    - 4872502bff61fe8feb9f0f99b0c9cdaa95d8038e
    - 6782a70a92f55e1e91eaf39285f25b48ff19419c
    
    Trunk is now synced with `6.9-alpha-61028`
    
    See #121

--- wp-admin/includes/class-theme-upgrader.php
+++ wp-admin/includes/class-theme-upgrader.php
@@ -313,7 +313,7 @@ class Theme_Upgrader extends WP_Upgrader {
 			return false;
 		}
 
-		$r = $current->response[ $theme ];
+		$upgrade_data = $current->response[ $theme ];
 
 		add_filter( 'upgrader_pre_install', array( $this, 'current_before' ), 10, 2 );
 		add_filter( 'upgrader_post_install', array( $this, 'current_after' ), 10, 2 );
@@ -325,7 +325,7 @@ class Theme_Upgrader extends WP_Upgrader {
 
 		$this->run(
 			array(
-				'package'           => $r['package'],
+				'package'           => $upgrade_data['package'],
 				'destination'       => get_theme_root( $theme ),
 				'clear_destination' => true,
 				'clear_working'     => true,
@@ -391,8 +391,8 @@ class Theme_Upgrader extends WP_Upgrader {
 		$this->skin->header();
 
 		// Connect to the filesystem first.
-		$res = $this->fs_connect( array( WP_CONTENT_DIR ) );
-		if ( ! $res ) {
+		$connected = $this->fs_connect( array( WP_CONTENT_DIR ) );
+		if ( ! $connected ) {
 			$this->skin->footer();
 			return false;
 		}
@@ -432,10 +432,10 @@ class Theme_Upgrader extends WP_Upgrader {
 			}
 
 			// Get the URL to the zip file.
-			$r = $current->response[ $theme ];
+			$upgrade_data = $current->response[ $theme ];
 
-			$requires_wp   = isset( $r['requires'] ) ? $r['requires'] : null;
-			$requires_r    = isset( $r['requires_r'] ) ? $r['requires_r'] : null;
+			$requires_wp   = isset( $upgrade_data['requires'] ) ? $upgrade_data['requires'] : null;
+			$requires_r    = isset( $upgrade_data['requires_r'] ) ? $upgrade_data['requires_r'] : null;
 			$is_compatible = is_wp_version_compatible( $requires_wp ) && is_retraceur_version_compatible( $requires_r );
 
 			if ( ! $is_compatible ) {
@@ -453,14 +453,14 @@ class Theme_Upgrader extends WP_Upgrader {
 				$this->skin->before( $result );
 				$this->skin->error( $result );
 				$this->skin->after();
-			} elseif ( isset( $r['requires_php'] ) && ! is_php_version_compatible( $r['requires_php'] ) ) {
+			} elseif ( isset( $upgrade_data['requires_php'] ) && ! is_php_version_compatible( $upgrade_data['requires_php'] ) ) {
 				$result = new WP_Error(
 					'incompatible_php_required_version',
 					sprintf(
 						/* translators: 1: Current PHP version, 2: PHP version required by the new theme version. */
 						__( 'The PHP version on your server is %1$s, however the new theme version requires %2$s.' ),
 						PHP_VERSION,
-						$r['requires_php']
+						$upgrade_data['requires_php']
 					)
 				);
 
@@ -471,7 +471,7 @@ class Theme_Upgrader extends WP_Upgrader {
 				add_filter( 'upgrader_source_selection', array( $this, 'check_package' ) );
 				$result = $this->run(
 					array(
-						'package'           => $r['package'],
+						'package'           => $upgrade_data['package'],
 						'destination'       => get_theme_root( $theme ),
 						'clear_destination' => true,
 						'clear_working'     => true,
@@ -568,7 +568,7 @@ class Theme_Upgrader extends WP_Upgrader {
 		}
 
 		// All these headers are needed on Theme_Installer_Skin::do_overwrite().
-		$info = get_file_data(
+		$new_theme_data = get_file_data(
 			$working_directory . 'style.css',
 			array(
 				'Name'        => 'Theme Name',
@@ -580,7 +580,7 @@ class Theme_Upgrader extends WP_Upgrader {
 			)
 		);
 
-		if ( empty( $info['Name'] ) ) {
+		if ( empty( $new_theme_data['Name'] ) ) {
 			return new WP_Error(
 				'incompatible_archive_theme_no_name',
 				$this->strings['incompatible_archive'],
@@ -598,7 +598,7 @@ class Theme_Upgrader extends WP_Upgrader {
 		 * - block themes require /templates/index.html or block-templates/index.html (deprecated 5.9.0).
 		 */
 		if (
-			empty( $info['Template'] ) &&
+			empty( $new_theme_data['Template'] ) &&
 			! file_exists( $working_directory . 'index.php' ) &&
 			! file_exists( $working_directory . 'templates/index.html' ) &&
 			! file_exists( $working_directory . 'block-templates/index.html' )
@@ -617,9 +617,9 @@ class Theme_Upgrader extends WP_Upgrader {
 			);
 		}
 
-		$requires_php  = isset( $info['RequiresPHP'] ) ? $info['RequiresPHP'] : null;
-		$requires_wp   = isset( $info['RequiresWP'] ) ? $info['RequiresWP'] : null;
-		$requires_r    = isset( $info['RequiresR'] ) ? $info['RequiresR'] : null;
+		$requires_php  = isset( $new_theme_data['RequiresPHP'] ) ? $new_theme_data['RequiresPHP'] : null;
+		$requires_wp   = isset( $new_theme_data['RequiresWP'] ) ? $new_theme_data['RequiresWP'] : null;
+		$requires_r    = isset( $new_theme_data['RequiresR'] ) ? $new_theme_data['RequiresR'] : null;
 		$is_compatible = is_wp_version_compatible( $requires_wp ) && is_retraceur_version_compatible( $requires_r );
 
 		if ( ! is_php_version_compatible( $requires_php ) ) {
@@ -644,7 +644,7 @@ class Theme_Upgrader extends WP_Upgrader {
 			return new WP_Error( 'incompatible_wp_required_version', $this->strings['incompatible_archive'], $error );
 		}
 
-		$this->new_theme_data = $info;
+		$this->new_theme_data = $new_theme_data;
 
 		return $source;
 	}
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.9-alpha-61022';
+$wp_version = '6.9-alpha-61028';
 
 /**
  * The Retraceur version string.
