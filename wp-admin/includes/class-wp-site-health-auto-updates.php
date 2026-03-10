09114e639b57b96f879b561ab6b94d544b958c78
imath <imathi.eu@outlook.fr>
2026-03-11 00:11:22 +0100

    Remove extra white spaces
    These were wrongly added in https://github.com/retraceur/coeur/commit/76ce971459cb299f7b2bf53f8761987ac064626e

--- wp-admin/includes/class-wp-site-health-auto-updates.php
+++ wp-admin/includes/class-wp-site-health-auto-updates.php
@@ -286,7 +286,7 @@ class WP_Site_Health_Auto_Updates {
 
 		/*
 		 * @todo Use GitHub assets generated checksums.
-		 * 
+		 *
 		 * Make sure the `get_core_checksums()` function is available during our REST API call.
 		if ( ! function_exists( 'get_core_checksums' ) ) {
 			require_once ABSPATH . 'wp-admin/includes/update.php';
@@ -319,7 +319,7 @@ class WP_Site_Health_Auto_Updates {
 		$unwritable_files = array();
 		/*
 		 * @todo Use GitHub assets generated checksums.
-		 * 
+		 *
 		 * foreach ( array_keys( $checksums ) as $file ) {
 			if ( str_starts_with( $file, 'wp-content' ) ) {
 				continue;
