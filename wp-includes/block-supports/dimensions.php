05842e29ea7eddd1f45a0a0bb4fbd5279ac7aa6f
imath <math.viet@gmail.com>
2026-03-26 03:31:14 +0100

    Add 5 commits from WP 7.0
    Commits list:
    - 74d6d906f268d13560663e133608fddee984b036
    - 6a9a061a04dc168a7a10ccf5b0e37983c100ee63
    - 527ccba62f72bfbb0e59fd0db6ca76c6c27a8603
    - 45d10c626c295c3b078b8d97af9cc60be43394d8
    - de5f29a04a8dfd97a4b9b30b177c5d82445aa387
    
    Trunk is synced with 7.0-alpha-61436
    
    See #171

--- wp-includes/block-supports/dimensions.php
+++ wp-includes/block-supports/dimensions.php
@@ -62,7 +62,7 @@ function wp_apply_dimensions_support( $block_type, $block_attributes ) {
 	// Width support to be added in near future.
 
 	$has_min_height_support = block_has_support( $block_type, array( 'dimensions', 'minHeight' ), false );
-	$block_styles           = isset( $block_attributes['style'] ) ? $block_attributes['style'] : null;
+	$block_styles           = $block_attributes['style'] ?? null;
 
 	if ( ! $block_styles ) {
 		return $attributes;
@@ -72,9 +72,7 @@ function wp_apply_dimensions_support( $block_type, $block_attributes ) {
 	$dimensions_block_styles              = array();
 	$dimensions_block_styles['minHeight'] = null;
 	if ( $has_min_height_support && ! $skip_min_height ) {
-		$dimensions_block_styles['minHeight'] = isset( $block_styles['dimensions']['minHeight'] )
-			? $block_styles['dimensions']['minHeight']
-			: null;
+		$dimensions_block_styles['minHeight'] = $block_styles['dimensions']['minHeight'] ?? null;
 	}
 	$styles = wp_style_engine_get_styles( array( 'dimensions' => $dimensions_block_styles ) );
 
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61428';
+$wp_version = '7.0-alpha-61436';
 
 /**
  * The Retraceur version string.
