e7d701bc28ed776814e0d7d9106a2a419040f148
imath <math.viet@gmail.com>
2026-04-04 22:30:17 +0200

    Add 5 commits from WP 7.0
    Commits list:
    - 612813b181e1bccfbd57c328185e9826b1e1f2e3
    - 6dbbf09e66da1035c7c1c0dd23bd046bea22c901
    - 30895b5a2c415f4883e7f92e770a3f7031014bdd
    - 6fa9fcb3dc4d57d2b87d42cf78f0c2a529285d29
    - bc89803296568e955487c7eb0f5c20811f586fc9
    
    Trunk is synced with 7.0-alpha-61512
    
    See #171

--- wp-includes/abilities-api/class-wp-ability.php
+++ wp-includes/abilities-api/class-wp-ability.php
@@ -105,7 +105,7 @@ class WP_Ability {
 	 * The ability execute callback.
 	 *
 	 * @since WP 6.9.0
-	 * @var callable( mixed $input= ): (mixed|WP_Error)
+	 * @var callable(mixed): (mixed|WP_Error)
 	 */
 	protected $execute_callback;
 
@@ -113,7 +113,7 @@ class WP_Ability {
 	 * The optional ability permission callback.
 	 *
 	 * @since WP 6.9.0
-	 * @var callable( mixed $input= ): (bool|WP_Error)
+	 * @var callable(mixed): (bool|WP_Error)
 	 */
 	protected $permission_callback;
 
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-alpha-61501';
+$wp_version = '7.0-alpha-61512';
 
 /**
  * The Retraceur version string.
