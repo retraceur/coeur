100dbd5c3ababdb0d25bc293225548809a9f90c4
imath <math.viet@gmail.com>
2026-05-10 06:03:05 +0200

    Add 5 commits from WP 7.0
    Commits list:
    - a67c640b9ba459c57c29ac9e83cb93de2440cbe2
    - 0c43da9ee143bc2033a0de7eef82baa214f158ad
    - bb66472f12d41b6418571d3225b888eaf47862a1
    - cf512a5f905de442469c9f4a80e0f087ade98b59
    - 4a1a88562bdfce748858b12e24353f2ce1910848
    
    Trunk is now synced with 7.0-beta4-61981
    
    See #171

--- wp-admin/includes/file.php
+++ wp-admin/includes/file.php
@@ -1237,7 +1237,7 @@ function _unzip_file_pclzip( $file, $to, $needed_dirs = array() ) {
 		}
 	}
 
-	/** This filter is documented in src/wp-admin/includes/file.php */
+	/** This filter is documented in wp-admin/includes/file.php */
 	$pre = apply_filters( 'pre_unzip_file', null, $file, $to, $needed_dirs, $required_space );
 
 	if ( null !== $pre ) {
@@ -1264,7 +1264,7 @@ function _unzip_file_pclzip( $file, $to, $needed_dirs = array() ) {
 		}
 	}
 
-	/** This action is documented in src/wp-admin/includes/file.php */
+	/** This filter is documented in wp-admin/includes/file.php */
 	$result = apply_filters( 'unzip_file', true, $file, $to, $needed_dirs, $required_space );
 
 	unset( $needed_dirs );
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-beta4-61940';
+$wp_version = '7.0-beta4-61981';
 
 /**
  * The Retraceur version string.
