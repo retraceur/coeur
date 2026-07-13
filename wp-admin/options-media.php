3208e6471ff23c2c3d114fac26d6c4c77241d0bf
imath <math.viet@gmail.com>
2026-07-13 17:09:01 +0200

    Apply WP 7.0 site-icon CSS improvements
    Report these improvements in og-image.
    
    See #171

--- wp-admin/options-media.php
+++ wp-admin/options-media.php
@@ -156,7 +156,7 @@ require_once ABSPATH . 'wp-admin/admin-header.php';
 					<button
 						id="js-remove-site-icon"
 						type="button"
-						<?php echo has_site_icon() ? 'class="button button-secondary reset remove-site-icon"' : 'class="button button-secondary reset hidden"'; ?>
+						<?php echo has_site_icon() ? 'class="button button-secondary reset remove-site-icon"' : 'class="button button-secondary reset remove-site-icon hidden"'; ?>
 					>
 						<?php _e( 'Remove Site Icon' ); ?>
 					</button>
--- wp-includes/assets/global-media.asset.php
+++ wp-includes/assets/global-media.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => '8af6bc0512937d9e7e2b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-media-utils'), 'version' => '02b338ce69eb0522c3f4');
