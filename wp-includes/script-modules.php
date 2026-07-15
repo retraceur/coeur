7370bc9aec77984984dc1b9d3d3d2337ac1c9bab
imath <imathi.eu@outlook.fr>
2026-07-15 16:09:02 +0200

    Interactivity API: fix an issue with `@wp/interactivity` loading
    I missed the changes added into this WP 7.0 commit: f8aecf9607046a4e91faeb35bf390d7b95b1fa0d
    
    See #171

--- wp-includes/script-modules.php
+++ wp-includes/script-modules.php
@@ -171,9 +171,9 @@ function wp_default_script_modules() {
 	/*
 	 * Expects multidimensional array like:
 	 *
-	 *     'interactivity/index.min.js' => array('dependencies' => array(…), 'version' => '…'),
-	 *     'interactivity/debug.min.js' => array('dependencies' => array(…), 'version' => '…'),
-	 *     'interactivity-router/index.min.js' => …
+	 *     'interactivity/index.js' => array('dependencies' => array(…), 'version' => '…'),
+	 *     'interactivity-router/index.js' => array('dependencies' => array(…), 'version' => '…'),
+	 *     'block-library/navigation/view.js' => …
 	 */
 	$assets_file = ABSPATH . WPINC . '/assets/script-modules-packages.php';
 	$assets      = file_exists( $assets_file ) ? include $assets_file : array();
@@ -182,29 +182,11 @@ function wp_default_script_modules() {
 		/*
 		 * Build the Retraceur Script Module ID from the file name.
 		 * Prepend `@wordpress/` and remove extensions and `/index` if present:
-		 *   - interactivity/index.min.js  => @wordpress/interactivity
-		 *   - interactivity/debug.min.js  => @wordpress/interactivity/debug
-		 *   - block-library/query/view.js => @wordpress/block-library/query/view
+		 *   - interactivity/index.min.js         => @wordpress/interactivity
+		 *   - interactivity-router/index.min.js  => @wordpress/interactivity-router
+		 *   - block-library/navigation/view.js   => @wordpress/block-library/navigation/view
 		 */
 		$script_module_id = '@wordpress/' . preg_replace( '~(?:/index)?(?:\.min)?\.js$~D', '', $file_name, 1 );
-
-		switch ( $script_module_id ) {
-			/*
-			 * Interactivity exposes two entrypoints, "/index" and "/debug".
-			 * "/debug" should replace "/index" in development.
-			 */
-			case '@wordpress/interactivity/debug':
-				if ( ! SCRIPT_DEBUG ) {
-					continue 2;
-				}
-				$script_module_id = '@wordpress/interactivity';
-				break;
-			case '@wordpress/interactivity':
-				if ( SCRIPT_DEBUG ) {
-					continue 2;
-				}
-				break;
-		}
 
 		/*
 		 * The Interactivity API is designed with server-side rendering as its primary goal, so all of its script modules
