926a31882c5ab3613f7647b008ddeb66ceaa5354
imath <math.viet@gmail.com>
2026-07-20 12:45:27 +0200

    Add missing admin & includes refreshed minified CSS

--- wp-includes/css/webpack.config.js
+++ wp-includes/css/webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
 			'buttons.min': './wp-includes/css/buttons.js',
 			'buttons-rtl.min': './wp-includes/css/buttons-rtl.js',
 			'media-views-rtl.min': './wp-includes/css/media-views-rtl.js',
-			'media-views.min': './wp-includes/css/media-views.js'
+			'media-views.min': './wp-includes/css/media-views.js',
+			'wp-auth-check-rtl.min': './wp-includes/css/wp-auth-check-rtl.js',
+			'wp-auth-check.min': './wp-includes/css/wp-auth-check.js',
+			'wp-pointer-rtl.min': './wp-includes/css/wp-pointer-rtl.js',
+			'wp-pointer.min': './wp-includes/css/wp-pointer.js'
         },
 		output: {
 			filename: '[name].js',
