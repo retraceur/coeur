61a96f4cc87998d2a7e1a261265c86b843eb3a1d
imath <math.viet@gmail.com>
2026-08-03 19:32:37 +0200

    Do 4.0.0-RC1 version bumps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
+## [4.0.0-RC1] - 2026-08-03
+
+
 ## [4.0.0-beta3] - 2026-08-01
 
 ### Changed
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -27,7 +27,7 @@ $wp_version = '7.0.3-alpha-62793';
  *
  * @global string $retraceur_version
  */
-$retraceur_version = '4.0.0-beta3';
+$retraceur_version = '4.0.0-RC1';
 
 /**
  * Holds the WP DB revision, increments when changes are made to the WP DB schema.
