7080b4d90cc37855e85fd0245b6e7b4c9ef8a1ef
imath <math.viet@gmail.com>
2026-08-06 22:18:52 +0200

    Apply WP 7.0.3 security fixes
    Commits list:
    - 19871635330b29253abd4cbc0114bdb0b2583037
    - 6764caea693694e8ebc6d1b86b7db02bf31db675
    - ed513016ef042488442d390f2649b3666ebf40c1
    - 04ee0e71603c08918bc059ee6ec728a9c85c7516
    - be9f46b84f1316975ff1cf8cb48b805e4150db39
    - 1d8216dc10e7cb8d3ee50cafef48b02a06e87f80
    - 3375f2712974c22a34d5dc2ba0727c250877c6e8
    - b82af7f18a07d7d4490bbd1c0640437813f16c81
    - 3dc987655f466354022f0dc4b7f04ca898867f74
    
    This commit also do 4.1.0 version bumps and update the changelog.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.3-alpha-62793';
+$wp_version = '7.0.3';
 
 /**
  * The Retraceur version string.
@@ -27,7 +27,7 @@ $wp_version = '7.0.3-alpha-62793';
  *
  * @global string $retraceur_version
  */
-$retraceur_version = '4.0.0';
+$retraceur_version = '4.1.0';
 
 /**
  * Holds the WP DB revision, increments when changes are made to the WP DB schema.
