6fa5f2530f825ef0382258c4c35a1f59713f7d37
imath <math.viet@gmail.com>
2026-08-13 05:33:00 +0200

    Apply WP 7.0.4 security fix
     See WP commit: a50a9407c8911594104060ff9445412ca7b272a8
    
     This commit also do 4.2.0 version bumps and update the changelog.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0.3';
+$wp_version = '7.0.4';
 
 /**
  * The Retraceur version string.
@@ -27,7 +27,7 @@ $wp_version = '7.0.3';
  *
  * @global string $retraceur_version
  */
-$retraceur_version = '4.1.0';
+$retraceur_version = '4.2.0';
 
 /**
  * Holds the WP DB revision, increments when changes are made to the WP DB schema.
