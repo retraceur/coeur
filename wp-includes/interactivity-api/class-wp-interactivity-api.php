79833b557a53bf29b54987b79365da550c04ba51
imath <math.viet@gmail.com>
2026-08-22 13:51:10 +0200

    Cherry pick 5 WP 7.1 commits
    Commits list:
    
    - fb38c37b1099c99e1d0c37aaffed22bb93666174
    - e24894c057486a2efa37a14b551467ad312d861e
    - 621a1fa01a67a4331b799bd390d8e65beb94e75b
    - 651f2044dd0d0b37ac303367a5c1e769965b91fc
    - beb8035db4c3dc838db4c4e47373275bf7f6db5a
    
    Trunk is now synced with WP 7.1-alpha-62460
    
    See #252

--- wp-includes/interactivity-api/class-wp-interactivity-api.php
+++ wp-includes/interactivity-api/class-wp-interactivity-api.php
@@ -140,8 +140,8 @@ final class WP_Interactivity_API {
 	 * @since WP 6.5.0
 	 * @since WP 6.6.0 The `$store_namespace` param is optional.
 	 *
-	 * @param string $store_namespace Optional. The unique store namespace identifier.
-	 * @param array  $state           Optional. The array that will be merged with the existing state for the specified
+	 * @param string|null $store_namespace Optional. The unique store namespace identifier.
+	 * @param array|null  $state           Optional. The array that will be merged with the existing state for the specified
 	 *                                store namespace.
 	 * @return array The current state for the specified store namespace. This will be the updated state if a $state
 	 *               argument was provided.
@@ -313,7 +313,7 @@ final class WP_Interactivity_API {
 	 *
 	 * @since WP 6.6.0
 	 *
-	 * @param string $store_namespace Optional. The unique store namespace identifier.
+	 * @param string|null $store_namespace Optional. The unique store namespace identifier.
 	 */
 	public function get_context( ?string $store_namespace = null ): array {
 		if ( null === $this->context_stack ) {
--- wp-includes/version.php
+++ wp-includes/version.php
@@ -18,7 +18,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.1-alpha-62454';
+$wp_version = '7.1-alpha-62460';
 
 /**
  * The Retraceur version string.
